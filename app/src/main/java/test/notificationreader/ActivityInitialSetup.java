93490a50dd18e597fd853631fed1199e5c5a7cc6
Daniel Gomez Rico <danielgomezrico@gmail.com>
2015-10-11 20:15:45 -0500

    Revert "Add translator to get text language automatically."
    This reverts commit 98a3c1e52ef08a42cfe3e0a1f65612834fc7f38c.

--- app/src/main/java/test/notificationreader/ActivityInitialSetup.java
+++ app/src/main/java/test/notificationreader/ActivityInitialSetup.java
@@ -3,6 +3,7 @@ package test.notificationreader;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -19,11 +20,11 @@ public class ActivityInitialSetup extends Activity {
     @OnClick(R.id.initial_button_permission)
     public void askPermissions() {
         Intent intent;
-//        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
-        intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
-//        } else {
-//            intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
-//        }
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN_MR2) {
+            intent = new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS");
+        } else {
+            intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+        }
         startActivity(intent);
     }
 

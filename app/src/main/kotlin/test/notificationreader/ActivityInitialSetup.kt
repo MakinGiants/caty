b0aee886b15a6b070fb270defa80201b9731e76d
Daniel Gomez Rico <danielgomezrico@gmail.com>
2016-02-07 10:08:28 -0500

    F notification try button is not working.

--- app/src/main/kotlin/test/notificationreader/ActivityInitialSetup.kt
+++ app/src/main/kotlin/test/notificationreader/ActivityInitialSetup.kt
@@ -6,7 +6,7 @@ import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.support.v4.app.NotificationCompat
+import android.support.v7.app.NotificationCompat
 import butterknife.ButterKnife
 import butterknife.OnClick
 
